080f982b72aa430ca47dd2bfe4a73f49b89c43ae
nlicalzi <nicholas.licalzi@gmail.com>
2020-12-28 09:24:49 -0500

    outlined necessary rendering functionality in View class

---
+++ public/javascripts/view.js
@@ -0,0 +1,16 @@
+class View {
+  // state
+  // behavior
+  
+  static renderContactsList(contacts) {} // either empty or populated
+  static renderSearchedContacts(contacts) {}
+  static renderNewContactForm() {}
+  static renderEditContactForm() {}
+  static showNewContactForm() {
+    // jQuery slide-down
+  }
+  static hideNewContactForm() {
+    // jQuery slide-up
+  }
+  static renderTags() {}
+}
